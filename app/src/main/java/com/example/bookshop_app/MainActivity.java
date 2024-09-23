26744259ec972579c0eb4c6946634e2b6f883298
Nguyễn Mạnh Tiến <tiennguyenmanh131202@gmail.com>
2024-09-23 22:40:48 +0700

    create project, create sign up and sign in fragment

---
+++ app/src/main/java/com/example/bookshop_app/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.bookshop_app;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.bookshop_app.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        assert navHostFragment != null;
+        NavController navController = navHostFragment.getNavController();
+
+        binding.bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+                if(itemId == R.id.nav_home){
+                    navController.navigate(R.id.homeFragment);
+                    return true;
+                }
+                if(itemId == R.id.nav_account){
+                    navController.navigate(R.id.profileFragment);
+                    return true;
+                }
+                if(itemId == R.id.nav_notification){
+                    navController.navigate(R.id.notificationFragment);
+                    return true;
+                }
+                if(itemId == R.id.nav_cart){
+                    navController.navigate(R.id.cartFragment);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/bookshop_app/fragments/auth/SignUpFragment.java
@@ -0,0 +1,89 @@
+package com.example.bookshop_app.fragments.auth;
+
+import android.graphics.Typeface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.text.InputType;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bookshop_app.R;
+import com.example.bookshop_app.databinding.FragmentSignUpBinding;
+
+public class SignUpFragment extends Fragment {
+
+    private FragmentSignUpBinding binding;
+
+    public SignUpFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentSignUpBinding.inflate(getLayoutInflater(), container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        NavController navController = Navigation.findNavController(view);
+
+        binding.btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.popBackStack();
+            }
+        });
+
+        // Hiển thị password
+        binding.btnViewPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.btnViewPassword.setVisibility(View.INVISIBLE);
+                binding.btnHidePassword.setVisibility(View.VISIBLE);
+
+                Typeface currentFont = binding.txtPassword.getTypeface();
+                binding.txtPassword.setInputType(InputType.TYPE_CLASS_TEXT);
+                binding.txtPassword.setTypeface(currentFont);
+                binding.txtPassword.setSelection(binding.txtPassword.length());
+            }
+        });
+
+        // Ẩn password
+        binding.btnHidePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.btnViewPassword.setVisibility(View.VISIBLE);
+                binding.btnHidePassword.setVisibility(View.INVISIBLE);
+
+                Typeface currentFont = binding.txtPassword.getTypeface();
+                binding.txtPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                binding.txtPassword.setTypeface(currentFont);
+                binding.txtPassword.setSelection(binding.txtPassword.length());
+            }
+        });
+
+        binding.btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.action_signUpFragment_to_signInFragment);
+            }
+        });
+
+    }
+}
